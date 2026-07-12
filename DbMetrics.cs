471dbe1b707ff052f29b8424e6f342c452afff1f
Kamil <56508385+kamil1722@users.noreply.github.com>
2026-07-12 22:00:37 +0300

    Update DbMetrics.cs

--- DbMetrics.cs
+++ DbMetrics.cs
@@ -2,8 +2,17 @@ using Prometheus;
 
 namespace EFMonitor
 {
-    public static class DbErrorRateMetrics
+    public static class DbMetrics
     {
+        public static readonly Counter TotalQueries =
+            Metrics.CreateCounter("db_queries_total", "Общее количество SQL-запросов");
+
+        public static readonly Counter FailedQueries =
+            Metrics.CreateCounter("db_queries_failed_total", "Количество упавших SQL-запросов");
+
+        public static readonly Counter LongTimeQueries =
+            Metrics.CreateCounter("db_queries_longtime_total", "Количество медленных SQL-запросов(>=1сек)");
+
         public static void RegisterQuery(bool failed, bool longTimeQuery)
         {
             DbMetrics.TotalQueries.Inc();
@@ -18,17 +27,5 @@ namespace EFMonitor
                 DbMetrics.LongTimeQueries.Inc();
             }
         }
-    }
-
-    public static class DbMetrics
-    {
-        public static readonly Counter TotalQueries =
-            Metrics.CreateCounter("db_queries_total", "Общее количество SQL-запросов");
-
-        public static readonly Counter FailedQueries =
-            Metrics.CreateCounter("db_queries_failed_total", "Количество упавших SQL-запросов");
-
-        public static readonly Counter LongTimeQueries =
-            Metrics.CreateCounter("db_queries_longtime_total", "Количество медленных SQL-запросов(>=1сек)");
     }
 }
